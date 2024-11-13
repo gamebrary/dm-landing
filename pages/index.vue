88e014c47aabc0c2e3b3b6300b5c04f19f608a56
Gamebrary <gamebraryhq@gmail.com>
2024-11-13 15:03:41 -0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="landing">
+    <h1>Welcome to Dugout Master</h1>
+    <p>Master the Dugout, Master the Game.</p>
+    <button @click="learnMore">Learn More</button>
+  </div>
+</template>
+
+<script setup>
+const learnMore = () => {
+  alert("Coming soon!");
+}
+</script>
+
+<style>
+.landing {
+  text-align: center;
+  padding: 50px;
+  font-family: Arial, sans-serif;
+}
+</style>
